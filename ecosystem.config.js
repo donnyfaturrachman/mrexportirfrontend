ec35dca980c09332d86420c68409bd2f862d852f
bayu <bayu@katadata.co.id>
2020-09-17 13:19:06 +0700

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps: [{
-        name: 'user-dashboard',
+        name: 'NuxtAppName',
         exec_mode: 'cluster',
         instances: 'max', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
